7a4731e99857101af105e31b6f27a65bd29fa0a0
rrong777 <wql789@qq.com>
2021-11-07 21:01:33 +0800

    不带分区号，使用key计算hash获得分区号

--- kafka-demo/src/main/java/com/wuqr/producer/CallbackProducer.java
+++ kafka-demo/src/main/java/com/wuqr/producer/CallbackProducer.java
@@ -2,6 +2,8 @@ package com.wuqr.producer;
 
 import org.apache.kafka.clients.producer.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -21,11 +23,15 @@ public class CallbackProducer {
 
         // 2. 创建生产者对象
         KafkaProducer<String,String> kafkaProducer = new KafkaProducer<>(properties);
-
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
         // 3. 发送数据 直接new 接口 就是匿名实现类的匿名对象
         for (int i = 0; i < 10; i++) {
             // 分区号0之后的atguigu是消息的key
-            kafkaProducer.send(new ProducerRecord<>("first", 0, "atguigu", "atguigu--" + i), (metadata, exception) -> {
+            // 如果不带分区号，就给一个key 是按照key的哈希值去算分区号
+            kafkaProducer.send(new ProducerRecord<>("first",  list.get(i % 3), "atguigu--" + i), (metadata, exception) -> {
                 // 成功了返回的就是metadata 失败了返回的就是异常
                 // 新创建一个主题，发送消息，可以看到offset是从0开始计算的
                 if(exception == null) {
